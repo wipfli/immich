e860526145112601c50787104ba4a1af43f228b4
Alex Tran <alex.tran1502@gmail.com>
2023-08-18 11:19:24 -0500

    send store event to page

--- web/src/routes/(user)/photos/+page.svelte
+++ web/src/routes/(user)/photos/+page.svelte
@@ -16,8 +16,10 @@
   import { AssetAction } from '$lib/constants';
   import { createAssetInteractionStore } from '$lib/stores/asset-interaction.store';
   import { AssetStore } from '$lib/stores/assets.store';
+  import { websocketStore } from '$lib/stores/websocket';
   import { openFileUploadDialog } from '$lib/utils/file-uploader';
   import { TimeBucketSize } from '@api';
+  import { onDestroy } from 'svelte';
   import DotsVertical from 'svelte-material-icons/DotsVertical.svelte';
   import Plus from 'svelte-material-icons/Plus.svelte';
   import type { PageData } from './$types';
@@ -29,6 +31,13 @@
   const { isMultiSelectState, selectedAssets } = assetInteractionStore;
 
   $: isAllFavorite = Array.from($selectedAssets).every((asset) => asset.isFavorite);
+
+  const wsPageUnsubscriber = websocketStore.onUploadSuccess.subscribe((asset) => {
+    if (!asset) return;
+    console.log('I got a new asset upload here', asset);
+  });
+
+  onDestroy(() => wsPageUnsubscriber());
 </script>
 
 <UserPageLayout user={data.user} hideNavbar={$isMultiSelectState} showUploadButton>
